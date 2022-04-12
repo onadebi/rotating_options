621151453185edab3f1ec4b50cd0495921d896a2
Onaefe <onaefe@onaxsys.com>
2022-04-12 17:04:56 +0100

    Completion of rotating search results into options

--- src/components/OptionsSearch.tsx
+++ src/components/OptionsSearch.tsx
@@ -1,3 +1,4 @@
+import { debug } from "console";
 import React, { FormEvent } from "react";
 import agent from "../api/agent";
 import { ApiResponse } from "../models/ApiResponse";
@@ -6,32 +7,47 @@ const OptionsSearch = () => {
   const optionInit = ["A", "B", "C", "D", "E"];
   const [search, SetSearch] = React.useState("");
   const [options, SetOptions] = React.useState(optionInit);
-  const [searchResult, SetSearchResult]= React.useState<string[]>([]);
+  const [index, SetIndex] = React.useState(0);
+  const [searchResult, SetSearchResult] = React.useState<string[]>([]);
 
-  const handleSearchChange = async (e: FormEvent<HTMLInputElement>)=>{
+  const handleSearchChange = async (e: FormEvent<HTMLInputElement>) => {
     e.preventDefault();
     SetSearch(e.currentTarget.value);
     const result = await agent.SearchResult.get<ApiResponse>(search);
     const searchResults = result.results;
-    if(searchResults.length > 1){
-      const collectionNames = searchResults.map((item)=> (item.collectionName)).sort().slice(0,5);
+    if (searchResults.length > 1) {
+      const collectionNames = searchResults
+        .map((item) => item.collectionName)
+        .sort()
+        .slice(0, 5);
       SetSearchResult(collectionNames);
-      console.log(JSON.stringify(searchResult,null,2))
+      console.log("Returned resp >> ", JSON.stringify(searchResult, null, 2));
     }
-  }
-
-  const handleSubmit = (e: FormEvent) => {
-    e.preventDefault();
   };
+
+  // const handleSubmit = (e: FormEvent) => {
+  //   e.preventDefault();
+  // };
 
   React.useEffect(() => {
     const optionsChange = setInterval(() => {
-      const newOptions = [...options];
-      
-      if(searchResult.length > 1){
-        newOptions.push(newOptions.shift()!);
-        SetOptions(searchResult);
-      }else{
+      let newOptions = [...options];
+
+      if (searchResult.length > 1) {
+        console.log("Search result for replacement ", searchResult);
+        debugger;
+        const searchResponse = [...searchResult];
+        debugger;
+        newOptions.shift()!;
+        debugger;
+        newOptions.push(searchResult[index]);
+
+        SetOptions(newOptions);
+        debugger;
+        const indexValue = index < searchResult.length - 1 ? index + 1 : 0;
+        console.log("INdex Value is ", indexValue);
+        SetIndex(indexValue);
+      } else {
         if (newOptions.length > 1) {
           newOptions.push(newOptions.shift()!);
           SetOptions(newOptions);
@@ -41,11 +57,17 @@ const OptionsSearch = () => {
     return () => {
       clearInterval(optionsChange);
     };
-  }, [options, searchResult]);
+  }, [index, options, searchResult]);
 
   return (
     <>
-      <form onSubmit={ handleSubmit}>
+      <div
+        style={{
+          width: "70%",
+          margin: "auto",
+          textAlign: "center",
+        }}
+      >
         <br />
         <input
           type="text"
@@ -53,25 +75,27 @@ const OptionsSearch = () => {
           name="search"
           value={search}
           onChange={handleSearchChange}
-          placeholder="Search item"
+          onBlur={handleSearchChange}
+          placeholder="Search Band"
+          style={{width:'100%'}}
         />
         <br />
         {options.map((o, i) => {
           return (
-            <span
+            <div
               key={i}
               style={{
-                width: "50%",
+                width: "100%",
                 margin: " 0.5em auto",
                 textAlign: "center",
                 border: "1px solid gray",
               }}
             >
               {o}
-            </span>
+            </div>
           );
         })}
-      </form>
+      </div>
     </>
   );
 };
